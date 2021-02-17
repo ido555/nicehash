8c6e238899c5b5abcd384a10feaee0928a93ab1b
ido555 <13142010+ido555@users.noreply.github.com>
2021-02-17 19:56:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,41 +1,36 @@
 import requests
 from datetime import datetime
 
-class Algo:
-      def __init__(self, title, displayMiningFactor, displayMarketFactor, minimalOrderAmount,
-       minSpeedLimit, maxSpeedLimit, priceDownStep, minimalPoolDifficulty, port):
-       self.title = title
-       self.displayMiningFactor = displayMiningFactor
-       self.displayMarketFactor = displayMarketFactor
-       self.minimalOrderAmount = minimalOrderAmount
-       self.minSpeedLimit = minSpeedLimit
-       self.maxSpeedLimit = maxSpeedLimit
-       self.priceDownStep = priceDownStep
-       self.minimalPoolDifficulty = minimalPoolDifficulty
-       self.port = port
 
-       
-       
+class Algo:
+    def __init__(self, title, displayMiningFactor, displayMarketFactor, minimalOrderAmount,
+                 minSpeedLimit, maxSpeedLimit, priceDownStep, minimalPoolDifficulty, port):
+        self.title = title
+        self.displayMiningFactor = displayMiningFactor
+        self.displayMarketFactor = displayMarketFactor
+        self.minimalOrderAmount = minimalOrderAmount
+        self.minSpeedLimit = minSpeedLimit
+        self.maxSpeedLimit = maxSpeedLimit
+        self.priceDownStep = priceDownStep
+        self.minimalPoolDifficulty = minimalPoolDifficulty
+        self.port = port
 
 
 r = requests.get("https://api2.nicehash.com/api/v2/time")
-timeStamp = datetime.fromtimestamp(r.json()['serverTime']/1000)
+timeStamp = datetime.fromtimestamp(r.json()['serverTime'] / 1000)
 
 r = requests.get("https://api2.nicehash.com/main/api/v2/mining/algorithms")
 
-
 algosInfo = []
 for algoData in r.json()['miningAlgorithms']:
-    print (f"{algoData}\n")
+    print(f"{algoData}\n")
     title = algoData['title']
-    title = algoData['displayMiningFactor']
-    title = algoData['displayMarketFactor']
-    title = algoData['minimalOrderAmount']
-    title = algoData['minSpeedLimit']
-    title = algoData['maxSpeedLimit']
-    title = algoData['minimalPoolDifficulty']
-    title = algoData['port']
+    displayMiningFactor = algoData['displayMiningFactor']
+    displayMarketFactor = algoData['displayMarketFactor']
+    minimalOrderAmount = algoData['minimalOrderAmount']
+    minSpeedLimit = algoData['minSpeedLimit']
+    maxSpeedLimit = algoData['maxSpeedLimit']
+    priceDownStep = algoData['priceDownStep']
+    minimalPoolDifficulty = algoData['minimalPoolDifficulty']
+    port = algoData['port']
     algosInfo.append(Algo())
-
-    
-
